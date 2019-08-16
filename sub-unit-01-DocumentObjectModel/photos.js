51ac644860dfd20e1752bafff9e0ce712b3375b5
Evelyn <eduarte019@west-mec.org>
2019-08-15 18:04:21 -0700

    Step 2
    must finish step

--- sub-unit-01-DocumentObjectModel/photos.js
+++ sub-unit-01-DocumentObjectModel/photos.js
@@ -1,10 +1,8 @@
 /*    Exercise 01_05_01
-
  *    Photo gallery
  *    Variables and functions
  *    Author: Evelyn Duarte
  *    Date: 8/14/19
-
  *    Filename: photos.js
  */
 
@@ -23,45 +21,77 @@ function rightArrow() {
       }
       populateFigures();
    }
-   if(rightArrow.addEventListener){
-      rightArrow.addEventListener("click", rightArrow, false);
-   }else if (rightArrow.attachEvent){
-      rightArrow.attachEvent("onclick", rightArrow);
-   }
+   
 }
 
 /* shift all images one figure to the right, and change values in photoOrder array to match  */
 function leftArrow() {
-   for (var i = 0; i < 5; i++) {
-      if ((photoOrder[i] - 1) === 0) {
-         photoOrder[i] = 5;
-      } else {
-         photoOrder[i] -= 1;
-      }
-      populateFigures();
-   }
-   if(leftArrow.addEventListener){
-      leftArrow.addEventListener("click", leftArrow, false);
-   }else if (leftArrow.attachEvent){
-      leftArrow.attachEvent("onclick", leftArrow);
-   }
-}
-
-/* open center figure in separate window */
-function zoomFig() {
+   // for (var i = 0; i < 5; i++) {
+   //    if ((photoOrder[i] - 1) === 0) {
+   //       photoOrder[i] = 5;
+   //    } else {
+   //       photoOrder[i] -= 1;
+   //    }
+   //    populateFigures();
+   // }
+   alert("left asdfsgdsf");
    
 }
+
+function populateFigures() {
+   
+}
+
+function previewFive() {
+   
+}
+/* open center figure in separate window */
+
 
 /* create event listeners and populate image elements */
 function setUpPage() {
    createEventListeners();
    populateFigures();
 }
+
 function createEventListeners() {
 var leftArrow = document.getElementById("leftarrow");
-alert(leftArrow);
+
+if(leftarrow.addEventListener){
+   leftarrow.addEventListener("click", leftArrow, false);
+}else if (leftarrow.attachEvent){
+   leftarrow.attachEvent("onclick", leftArrow);
 }
 
+if(rightarrow.addEventListener){
+   rightarrow.addEventListener("click", rightArrow, false);
+}else if (rightarrow.attachEvent){
+   rightarrow.attachEvent("onclick", rightArrow);
+}
+
+var mainFig = document.getElementsByTagName("img")[1];
+
+if(mainFig.addEventListener){
+   mainFig.addEventListener("click", zoomFig, false);
+} else if (mainFig.attachEvent){
+   mainFig.attachEvent("onclick", zoomFig);
+}
+
+}
+
+function zoomFig() {
+   
+}
+
+
+function populateFigures() {
+   var filename;
+   var currentFig;
+
+   for(var i = 1; i < 4; i++){
+      
+   }
+}
 /* run setUpPage() function when page finishes loading */
 if (window.addEventListener) {
   window.addEventListener("load", setUpPage, false); 
